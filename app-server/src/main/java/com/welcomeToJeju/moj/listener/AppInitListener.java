a6f0dd6a38330038fa0ffd856590f847dab8ff57
leeseoeun <seoeun.shuen@gmail.com>
2021-11-07 01:18:46 +0900

    ,

--- app-server/src/main/java/com/welcomeToJeju/moj/listener/AppInitListener.java
+++ app-server/src/main/java/com/welcomeToJeju/moj/listener/AppInitListener.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.welcomeToJeju.moj.dao.ThemeDao;
+import com.welcomeToJeju.moj.dao.UserDao;
 
 @WebListener
 public class AppInitListener implements ServletContextListener {
@@ -24,6 +25,7 @@ public class AppInitListener implements ServletContextListener {
           "com/welcomeToJeju/moj/conf/mybatis-config.xml")).openSession();
 
       // SqlSession 객체를 통해 MemberDao 구현체를 자동 생성한다.
+      UserDao userDao = sqlSession.getMapper(UserDao.class);
       ThemeDao themeDao = sqlSession.getMapper(ThemeDao.class);
 
       // 모든 웹 애플리케이션의 컴포넌트(서블릿, 리스너, 필터)가 공유할 객체를 두는 저장소
@@ -31,6 +33,7 @@ public class AppInitListener implements ServletContextListener {
 
       // 웹 애플리케이션 공용 저장소에 DAO 객체를 보관한다.
       // => 이 저장소에 보관된 객체는 서블릿에서 사용할 것이다.
+      웹애플리케이션공용저장소.setAttribute("userDao", userDao);
       웹애플리케이션공용저장소.setAttribute("themeDao", themeDao);
 
       웹애플리케이션공용저장소.setAttribute("sqlSession", sqlSession);      
