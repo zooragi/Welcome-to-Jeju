82be20124c3716e2a299cca0de62aceaa1815f4f
leeseoeun <seoeun.shuen@gmail.com>
2021-11-09 22:40:46 +0900

    1109 테마 상세 보기에 닉네임 추가

--- app-server/src/main/java/com/welcomeToJeju/moj/servlet/theme/myTheme/MyThemeDetailController.java
+++ app-server/src/main/java/com/welcomeToJeju/moj/servlet/theme/myTheme/MyThemeDetailController.java
@@ -2,21 +2,22 @@ package com.welcomeToJeju.moj.servlet.theme.myTheme;
 
 import java.io.IOException;
 import java.util.Collection;
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.welcomeToJeju.moj.dao.PlaceDao;
 import com.welcomeToJeju.moj.dao.ThemeDao;
 import com.welcomeToJeju.moj.dao.UserDao;
 import com.welcomeToJeju.moj.domain.Place;
 import com.welcomeToJeju.moj.domain.Theme;
+import com.welcomeToJeju.moj.domain.User;
 
 @WebServlet("/mytheme/detail")
-public class MyThemeDetailController extends GenericServlet {
+public class MyThemeDetailController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   ThemeDao themeDao;
@@ -27,20 +28,25 @@ public class MyThemeDetailController extends GenericServlet {
   public void init(ServletConfig config) throws ServletException {
     ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
     themeDao = (ThemeDao) 웹애플리케이션공용저장소.getAttribute("themeDao");
+    userDao = (UserDao) 웹애플리케이션공용저장소.getAttribute("userDao");
     placeDao = (PlaceDao) 웹애플리케이션공용저장소.getAttribute("placeDao");
   }
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     try {
       int no = Integer.parseInt(request.getParameter("no"));
 
       Theme theme = themeDao.findByNo(no);
+      //      Category category = themeDao.findCategoryByNo(no);
+      User user = userDao.findByNo(theme.getOwner().getNo());
       Collection<Place> placeList = placeDao.findAllByThemeNo(no);
 
       request.setAttribute("theme", theme);
+      //      request.setAttribute("category", category);
+      request.setAttribute("user", user);
       request.setAttribute("placeList", placeList);
 
       request.getRequestDispatcher("/theme/myTheme/MyThemeDetail.jsp").forward(request, response);
