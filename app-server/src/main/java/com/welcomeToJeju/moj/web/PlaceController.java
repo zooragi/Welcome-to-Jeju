f81aae3ec37097705d6fa178f76370df850ab3ba
Bangwooju <phonestudio97@gmail.com>
2021-11-23 01:35:21 +0900

    ,

--- app-server/src/main/java/com/welcomeToJeju/moj/web/PlaceController.java
+++ app-server/src/main/java/com/welcomeToJeju/moj/web/PlaceController.java
@@ -2,11 +2,9 @@ package com.welcomeToJeju.moj.web;
 
 import java.util.HashMap;
 import java.util.UUID;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
-
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,14 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.google.gson.Gson;
 import com.welcomeToJeju.moj.dao.PlaceDao;
-import com.welcomeToJeju.moj.domain.Comment;
-import com.welcomeToJeju.moj.domain.Photo;
+import com.welcomeToJeju.moj.dao.ThemeDao;
 import com.welcomeToJeju.moj.domain.Place;
+import com.welcomeToJeju.moj.domain.Theme;
 import com.welcomeToJeju.moj.domain.User;
-
 import net.coobird.thumbnailator.ThumbnailParameter;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
@@ -33,16 +29,19 @@ import net.coobird.thumbnailator.name.Rename;
 @RequestMapping("/place")
 public class PlaceController {
 
-	@Autowired SqlSessionFactory sqlSessionFactory;
-	@Autowired ServletContext sc;
+  @Autowired SqlSessionFactory sqlSessionFactory;
+  @Autowired ServletContext sc;
   @Autowired PlaceDao placeDao;
+  @Autowired ThemeDao themeDao;  
   int themeNo = 0;
 
   @GetMapping("list")
   public ModelAndView list(String no) throws Exception{
     ModelAndView mv = new ModelAndView();
     themeNo = Integer.parseInt(no);
-    mv.setViewName("place/PlaceList2");
+    Theme theme = themeDao.findByNo(themeNo);
+    mv.addObject(theme);
+    mv.setViewName("place/PlaceList3");
     return mv;
   }
 
@@ -57,28 +56,28 @@ public class PlaceController {
     model.addAttribute("searchedPlace", keyword);
     return "place/PlaceSearch";
   }
-  
+
   @PostMapping(value="add")
   public String add(Place place, Part[] photoFile, HttpSession session,
-  		String comment) throws Exception{
-	
-	  	placeDao.insert(place);
-	  	
-	  	User user = (User) session.getAttribute("loginUser");
-		
-	  	HashMap<String,Object> param1 = new HashMap<>();
-	    HashMap<String,Object> param2 = new HashMap<>();
-	    HashMap<String,Object> param3 = new HashMap<>();
-	
-	    param3.put("themeNo", themeNo);
-	    param3.put("placeId", place.getId());
-	    param3.put("userNo", user.getNo());
-	    placeDao.insertPlaceUserTheme(param3);
+      String comment) throws Exception{
 
-      param1.put("placeId", place.getId());
-      param1.put("userNo", user.getNo());
-      param1.put("comment", comment);
-      placeDao.insertComment(param1);
+    placeDao.insert(place);
+
+    User user = (User) session.getAttribute("loginUser");
+
+    HashMap<String,Object> param1 = new HashMap<>();
+    HashMap<String,Object> param2 = new HashMap<>();
+    HashMap<String,Object> param3 = new HashMap<>();
+
+    param3.put("themeNo", themeNo);
+    param3.put("placeId", place.getId());
+    param3.put("userNo", user.getNo());
+    placeDao.insertPlaceUserTheme(param3);
+
+    param1.put("placeId", place.getId());
+    param1.put("userNo", user.getNo());
+    param1.put("comment", comment);
+    placeDao.insertComment(param1);
 
 
     for(Part p : photoFile) {
@@ -111,13 +110,13 @@ public class PlaceController {
         }
       });
       param2.put("filePath", filename);
-      
-      
+
+
       placeDao.insertPhoto(param2);
     }
     sqlSessionFactory.openSession().commit();
 
-  	return "redirect:list?no="+themeNo;
-    
+    return "redirect:list?no="+themeNo;
+
   }
 }
